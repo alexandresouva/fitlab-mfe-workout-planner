d657df14f9ba1c87f3d8497fdb27c4af609cce91
Alexandre Sousa Silva <pseudoalexandre@gmail.com>
2026-08-13 23:13:03 -0300

    feat(dx): implement autonomous execution and resolution of injector and port errors [closes #5]

---
+++ src/app/features/workout-planner/workout-planner.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-workout-planner',
+  standalone: true,
+  imports: [],
+  templateUrl: './workout-planner.component.html',
+  styleUrls: ['./workout-planner.component.scss']
+})
+export class WorkoutPlannerComponent {}
